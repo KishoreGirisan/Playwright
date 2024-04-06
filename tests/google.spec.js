708441340a487471762fe28e28d1a18b6d78b290
KishoreGirisan <kishoregirisan@gmail.com>
2024-04-06 17:33:51 -0400

    google site

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -1,10 +1,10 @@
 import {test,expect} from '@playwright/test'
 
 test("@Google UI check",async({page})=>{
+//Launch google
+    await page.goto('https://www.google.com/')
 
-await page.goto('https://www.google.com/')
-
-await page.locator('a[aria-label*="Gmail"]').isVisible()
+    await page.locator('a[aria-label*="Gmail"]').isVisible()
 
 
 })
