c5097cabe06374b237e81259b04588d7b0853659
KishoreGirisan <kishoregirisan@gmail.com>
2024-04-08 21:34:10 -0400

    test1

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -2,9 +2,9 @@ import {test,expect} from '@playwright/test'
 
 test("@Google UI check",async({page})=>{
 //Launch google
-    // await page.goto('https://www.google.com/')
+    await page.goto('https://www.google.com/')
 
-    // await page.locator('a[aria-label*="Gmail"]').isVisible()
+    await page.locator('a[aria-label*="Gmail"]').isVisible()
     console.log("hello, its my first docker test")
 
 
